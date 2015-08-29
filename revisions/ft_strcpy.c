63f1271739cf63377d5170ea530917208e613f2d
Myriam <mpillot@e1r3p20.42.fr>
2015-08-29 07:50:27 +0200

    lala

---
+++ revisions/ft_strcpy.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpillot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/08/28 12:54:42 by mpillot           #+#    #+#             */
+/*   Updated: 2015/08/28 15:03:00 by mpillot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void		ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void		ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+char		*ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int			main(void)
+{
+	char st[11] = "0123456789";
+	char ts[11];
+	ft_strcpy(ts, st);
+	ft_putstr(ts);
+	return (0);
+}
