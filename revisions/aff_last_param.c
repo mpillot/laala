8a59683c7c7fff0632b82675267b4911a09abf96
Myriam <mpillot@e1r3p20.42.fr>
2015-08-29 07:47:04 +0200

    lala

---
+++ revisions/aff_last_param.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_last_param.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpillot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/08/28 17:00:48 by mpillot           #+#    #+#             */
+/*   Updated: 2015/08/28 17:08:21 by mpillot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void		ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void		ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int			main(int argc, char **argv)
+{
+	if (argc > 1)
+		ft_putstr(argv[argc - 1]);
+	ft_putchar('\n');
+	return (0);
+}
